fdbe09bdc3d4896c7043fa4dc0a2dbdbe229eeaa
HenriqueGomes <henriquegomesjunqueira@gmail.com>
2023-07-06 21:32:10 -0300

    add window width

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import { ThemeProvider } from "@mui/material/styles";
 import { applicationtheme } from "./expanded-theme";
 import { DesktopLayout } from "./components/desktop/desktop-screen/desktop";
 import { Mobile } from "./components/mobile/mobile/mobile";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Update the window width when the viewport is resized
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <ThemeProvider theme={applicationtheme}>
-        {/* <DesktopLayout/> */}
-        <Mobile />
+       {windowWidth > 900 ? <DesktopLayout /> : <Mobile /> }
+       
       </ThemeProvider>
     </>
   );
